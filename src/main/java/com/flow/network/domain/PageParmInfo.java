748e6f6b826053d29a1f850910dde00e4d33738d
icefish <181712814@qq.com>
2023-06-25 17:27:06 +0800

    增加脚本导出功能

---
+++ src/main/java/com/flow/network/domain/PageParmInfo.java
@@ -0,0 +1,17 @@
+package com.flow.network.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class PageParmInfo {
+    private Integer pageNum;
+    private Integer pageSize;
+    private Integer pid;
+    private Integer pidNear;
+}
