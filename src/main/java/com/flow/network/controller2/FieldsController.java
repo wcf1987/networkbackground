31fb900035a46bbbac438ea4ac475d6a0c7fe32e
icefish <181712814@qq.com>
2023-10-30 17:36:14 +0800

    2.数据域导入功能。 3.新增接口 空白

--- src/main/java/com/flow/network/controller2/FieldsController.java
+++ src/main/java/com/flow/network/controller2/FieldsController.java
@@ -5,11 +5,10 @@ import com.flow.network.domain.PageParmInfo;
 import com.flow.network.domain2.FieldsEntity;
 import com.flow.network.domain2.MessBodyEntity;
 import com.flow.network.service2.FieldsServiceImp;
+import com.flow.network.tools.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/fields")
@@ -35,6 +34,21 @@ public class FieldsController {
     @PostMapping("/add")
     public ApiResponse add(@RequestBody FieldsEntity detailEntity){
         serviceImp.add(detailEntity);
+        return ApiResponse.success();
+    }
+
+    @PostMapping("/uploadfile")
+    public ApiResponse uploadfile(@RequestParam("file") MultipartFile[] file){
+
+        String path = "D:\\uploadfiles";
+        String result = "";
+        // 调用fileService保存文件
+        try {
+            result = Tools.storeFile(file[0], path);
+        }catch (Exception e){
+
+        }
+        //serviceImp.add(detailEntity);
         return ApiResponse.success();
     }
     @PostMapping("/update")
--- src/main/java/com/flow/network/tools/Tools.java
+++ src/main/java/com/flow/network/tools/Tools.java
@@ -1,7 +1,31 @@
 package com.flow.network.tools;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Tools {
     public static String SUCCESS="SUCCESS";
     public static String ERROR="ERROR";
 
+    public static String storeFile(MultipartFile file, String path) throws IllegalStateException, IOException {
+        // 获取原始文件名
+        String fileName = file.getOriginalFilename();
+        // // 获取文件后缀
+        // String suffixName = fileName.contains(".") ? fileName.substring(fileName.lastIndexOf(".")) : null;
+        // //文件的保存重新按照时间戳命名
+        // String newFileName = System.currentTimeMillis() + suffixName;
+        // 最终保存的文件
+        File filePath = new File(path + File.separator + fileName);
+        // 判断文件夹是否存在
+        if (!filePath.getParentFile().exists()) {
+            filePath.getParentFile().mkdirs();
+        }
+        // 保存文件
+        file.transferTo(filePath);
+        // 如果成功，会返回文件的绝对路径，方便下载
+        //logger.info("文件已保存至" +filePath.toString());
+        return filePath.toString();
+    }
 }
